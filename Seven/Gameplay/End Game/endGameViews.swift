4d6e0e5f2cdad953cb7a7174a03bf9da14cba35c
Marissa <1995marissapsy@live.com>
2020-07-13 13:06:11 +0800

    added app icon

--- Seven/Gameplay/End Game/endGameViews.swift
+++ Seven/Gameplay/End Game/endGameViews.swift
@@ -43,7 +43,7 @@ class EndGamePopupView : UIView {
         popupView.layer.cornerRadius = 15
         
         if newHighScore == true {
-            popupLabel.text = "Nice! You just got a new high score!"
+            popupLabel.text = "🎉🎉🎉 Nice! You just got a new high score! 🎉🎉🎉"
         } else {
             popupLabel.text = "Nooo - you're out of moves :'( "
 
